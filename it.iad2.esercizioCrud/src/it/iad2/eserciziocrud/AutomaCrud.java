fa8974aa5cf6e8ae37567191bb20bca20fcea3c3
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-12 17:43:58 +0100

    Nomi stati #2
    Nomi stati #2

--- it.iad2.esercizioCrud/src/it/iad2/eserciziocrud/AutomaCrud.java
+++ it.iad2.esercizioCrud/src/it/iad2/eserciziocrud/AutomaCrud.java
@@ -1,55 +1,100 @@
 package it.iad2.eserciziocrud;
 
 public class AutomaCrud implements State {
-    
+
     private State stato;
 
     public AutomaCrud() {
+        stato = new RicercaState();
     }
 
     @Override
     public void next(Event e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Siamo in" + stato);
+        System.out.println("Evento:" + e);
+        stato.next(e);
+        System.out.println("Arrivati in" + stato + "\n");
     }
-    
-    
-    public class Ricerca implements State{    
+
+    public class RicercaState implements State {
 
         @Override
         public void next(Event e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if (e instanceof AddEvent) {
+                stato = new AggiungiState();
+            } else if (e instanceof SelezionaEvent) {
+                stato = new VisualizzaState();
+            } else if (e instanceof RicercaEvent) {
+                System.out.println("Ho aggiornato la tabella");
+            } else {
+                System.out.println("Evento inatteso");
+            }
+
+        }
+
+    }
+
+    public class AggiungiState implements State {
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof AnnullaEvent) {
+                stato = new RicercaState();
+            } else if (e instanceof ConfermaEvent) {
+                stato = new VisualizzaState();
+            } else {
+                System.out.println("Evento inatteso");
+            }
+
         }
     }
-    
-    public class Aggiungi implements State{    
+
+    public class VisualizzaState implements State {
 
         @Override
         public void next(Event e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if (e instanceof AddEvent) {
+                stato = new AggiungiState();
+            } else if (e instanceof ModificaEvent) {
+                stato = new ModificaState();
+            } else if (e instanceof RimuoviEvent) {
+                stato = new RimuoviState();
+            } else if (e instanceof RicercaEvent) {
+                stato = new RicercaState();
+            } else if (e instanceof SelezionaEvent) {
+                stato = new VisualizzaState();
+            } else {
+                System.out.println("Evento inatteso");
+            }
         }
     }
-    
-    public class Visualizza implements State{    
+
+    public class ModificaState implements State {
 
         @Override
         public void next(Event e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if (e instanceof AnnullaEvent) {
+                stato = new VisualizzaState();
+            } else if (e instanceof ConfermaEvent) {
+                stato = new VisualizzaState();
+            } else {
+                System.out.println("Evento inatteso");
+            }
         }
     }
-    
-    public class Modifica implements State{    
+
+    public class RimuoviState implements State {
 
         @Override
         public void next(Event e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-    
-    public class Rimuovi implements State{    
+            if (e instanceof ConfermaEvent) {
+                stato = new RicercaState();
+            } else if (e instanceof AnnullaEvent) {
+                stato = new VisualizzaState();
+            } else {
+                System.out.println("Evento inatteso");
+            }
 
-        @Override
-        public void next(Event e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
     }
 }
